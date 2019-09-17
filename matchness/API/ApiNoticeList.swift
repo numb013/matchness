2894295215348043ed4aa83b54a2cb4e20024c70
中村篤史 <nakamuraatsushi@nakamuraatsushinoMacBook-Pro.local>
2019-09-17 16:04:31 +0900

    修正chatroomないとか

--- matchness/API/ApiNoticeList.swift
+++ matchness/API/ApiNoticeList.swift
@@ -1,10 +1,9 @@
-
 //
-//  ApiNoticeList.swift
-//  map-menu02
+//  ApiUserSearchData.swift
+//  matchness
 //
-//  Created by k1e091 on 2018/11/14.
-//  Copyright © 2018 nakajima. All rights reserved.
+//  Created by RW on 2019/07/10.
+//  Copyright © 2019 a2c. All rights reserved.
 //
 
 import Foundation;
@@ -15,12 +14,20 @@ import SwiftyJSON;
  構造体は利用時にプロパティ全てを初期化する必要がある。
  */
 public struct ApiNoticeList: CustomDebugStringConvertible {
-    
-    public var notice_id: Int? = nil;
+    public var id: Int? = nil;
     public var title: String? = nil;
-    public var body_text: String? = nil;
-    public var created_at: String? = nil;
-    
+    public var event_period: Int? = 0;
+    public var event_peple: Int? = 0;
+    public var master_id: Int? = nil;
+    public var start_type: Int? = 0;
+    public var present_point: Int? = 0;
+    public var status: Int? = 0;
+    public var event_type: Int? = 0;
+    public var event_start: String? = nil;
+    public var event_end: String? = nil;
+    public var progress_day: Int? = 0;
+    public var request_status: Int? = 0;
+    public var master_group: Int? = 0;
     /*
      デフォルトイニシャライザ
      失敗可能イニシャライザ init?
@@ -47,26 +54,64 @@ public struct ApiNoticeList: CustomDebugStringConvertible {
         if( item == JSON.null ){
             return nil;
         }
-        
         //String => String
-        if let notice_id = item["notice_id"].int {
-            self.notice_id = notice_id;
+        if let id = item["id"].int {
+            self.id = id;
         }
-        
         //String => String
         if let title = item["title"].string {
             self.title = title;
         }
-        
         //String => String
-        if let body_text = item["body_text"].string {
-            self.body_text = body_text;
+        if let event_peple = item["event_peple"].int {
+            self.event_peple = event_peple;
+        }
+        //String => String
+        if let event_period = item["event_period"].int {
+            self.event_period = event_period;
+        }
+        //String => String
+        if let master_id = item["master_id"].int {
+            self.master_id = master_id;
+        }
+        //String => String
+        if let start_type = item["start_type"].int {
+            self.start_type = start_type;
+        }
+        //String => String
+        if let present_point = item["present_point"].int {
+            self.present_point = present_point;
+        }
+        //String => String
+        if let status = item["status"].int {
+            self.status = status;
+        }
+        //String => String
+        if let event_type = item["event_type"].int {
+            self.event_type = event_type;
+        }
+        //String => String
+        if let event_start = item["event_start"].string {
+            self.event_start = event_start;
         }
         
         //String => String
-        if let created_at = item["created_at"].string {
-            self.created_at = created_at;
+        if let event_end = item["event_end"].string {
+            self.event_end = event_end;
         }
+        //String => String
+        if let progress_day = item["progress_day"].int {
+            self.progress_day = progress_day;
+        }
+        //String => String
+        if let request_status = item["request_status"].int {
+            self.request_status = request_status;
+        }
+        //String => String
+        if let master_group = item["master_group"].int {
+            self.master_group = master_group;
+        }
+        
         return true;
         
     }
@@ -76,11 +121,21 @@ public struct ApiNoticeList: CustomDebugStringConvertible {
      */
     public var debugDescription: String {
         get{
-            var string:String = "ApiNoticeList::\(#function)\n";
-            string += "notice_id => \(String(describing: self.notice_id))\n";
+            var string:String = "ApiGroupList::\(#function)\n";
+            string += "id => \(String(describing: self.id))\n";
             string += "title => \(String(describing: self.title))\n";
-            string += "body_text => \(String(describing: self.body_text))\n";
-            string += "created_at => \(String(describing: self.created_at))\n";
+            string += "event_peple => \(String(describing: self.event_peple))\n";
+            string += "event_period => \(String(describing: self.event_period))\n";
+            string += "master_id => \(String(describing: self.master_id))\n";
+            string += "start_type => \(String(describing: self.start_type))\n";
+            string += "present_point => \(String(describing: self.present_point))\n";
+            string += "status => \(String(describing: self.status))\n";
+            string += "event_type => \(String(describing: self.event_type))\n";
+            string += "event_start => \(String(describing: self.event_start))\n";
+            string += "event_end => \(String(describing: self.event_end))\n";
+            string += "progress_day => \(String(describing: self.progress_day))\n";
+            string += "request_status => \(String(describing: self.request_status))\n";
+            string += "master_group => \(String(describing: self.master_group))\n";
             return string;
         }
     }
---
+++ matchness/Group/GroupEvent/GroupChat/GroupChatViewController.swift
@@ -0,0 +1,159 @@
+//
+//  SettingEditViewController.swift
+//  matchness
+//
+//  Created by 中村篤史 on 2019/08/08.
+//  Copyright © 2019 a2c. All rights reserved.
+//
+
+import UIKit
+
+class GroupChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    let userDefaults = UserDefaults.standard
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cellCount: Int = 0
+    var dataSource: Dictionary<String, ApiGroupChatList> = [:]
+    var dataSourceOrder: Array<String> = []
+    
+    var selectRow = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+        
+        self.tableView.register(UINib(nibName: "SettingEditTableViewCell", bundle: nil), forCellReuseIdentifier: "SettingEditTableViewCell")
+        
+        /****************
+         APIへリクエスト（ユーザー取得）
+         *****************/
+        //ロジック生成
+        let requestGroupChatModel = GroupChatModel();
+        requestGroupChatModel.delegate = self as! GroupChatModelDelegate;
+        //リクエスト先
+        let requestUrl: String = ApiConfig.REQUEST_URL_API_SELECT_GROUP_CHAT;
+        //パラメーター
+        var query: Dictionary<String,String> = Dictionary<String,String>();
+        var api_key = userDefaults.object(forKey: "api_token") as? String
+        query["api_token"] = api_key
+        //リクエスト実行
+        if( !requestGroupChatModel.requestApi(url: requestUrl, addQuery: query) ){
+            
+        }
+        print("マイデータ")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "設定"
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+        header.tintColor = #colorLiteral(red: 0.9499146342, green: 0.9500735402, blue: 0.9498936534, alpha: 1)
+        header.textLabel?.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        header.textLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    }
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 35
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("こいいいいいい")
+        print(self.dataSource)
+        var myData = self.dataSource["0"]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingEditTableViewCell") as! SettingEditTableViewCell
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        cell.title?.text = "血液型"
+        //cell.detail?.text = ApiConfig.BLOOD_LIST[myData?.blood_type ?? 2]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("タップ")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+
+extension GroupChatViewController : GroupChatModelDelegate {
+    
+    func onStart(model: GroupChatModel) {
+        print("こちら/SettingEdit/UserDetailViewのonStart")
+    }
+    func onComplete(model: GroupChatModel, count: Int) {
+        print("UserDetail着てきてきてきて")
+        //更新用データを設定
+        self.dataSource = model.responseData;
+        self.dataSourceOrder = model.responseDataOrder;
+        
+        print(self.dataSourceOrder)
+        print("UserDetail耳耳耳意味耳みm")
+        print(self.dataSource)
+        
+        
+        //一つもなかったら
+        //        if( dataSourceOrder.isEmpty ){
+        //            return;
+        //        }
+        
+        //cellの件数更新
+        self.cellCount = dataSourceOrder.count;
+        //        self.cellCount = 10;
+        
+        
+        
+        //
+        var count: Int = 0;
+        //        for(key, code) in dataSourceOrder.enumerated() {
+        //            count+=1;
+        //            if let jenre: ApiUserDateParam = dataSource[code] {
+        //                //取得したデータを元にコレクションを再構築＆更新
+        //                mapMenuView.addTagGroup(model: model, jenre: jenre);
+        //            }
+        //        }
+        
+        tableView.reloadData()
+    }
+    func onFailed(model: GroupChatModel) {
+        print("こちら/ProfileEditModel/UserDetailViewのonFailed")
+    }
+    
+}
